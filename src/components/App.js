32860379d917d4f29841abae8823c13e5b338cad
Ariel Blasi <exorcismo@gmail.com>
2018-12-12 19:14:24 -0300

    Add form submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import "./App.css";
-import SearchBar from './SearchBar'
+import SearchBar from "./SearchBar";
 
 export default function App() {
 	return (
-		<div className="App">
-			<SearchBar></SearchBar>
+		<div className="ui container">
+			<SearchBar />
 		</div>
 	);
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function SearchBar(){
-	return(
-		<div>search bar</div>
-	)
+export default function SearchBar() {
+	const [input, setInput] = useState("enter any search term");
+
+	const onInputChange = e => {
+		setInput(e.target.value);
+		console.log(input);
+	};
+
+const onFormSubmit=(e)=>{
+	e.preventDefault()
+	//callback from parent component
+}
+
+	return (
+		<div className="search-bar ui segment">
+			<form onSubmit={onFormSubmit} className="ui form">
+				<div className="field">
+					<label>Video Search</label>
+					<input type="text" placeholder={input} onChange={onInputChange} />
+				</div>
+			</form>
+		</div>
+	);
 }
