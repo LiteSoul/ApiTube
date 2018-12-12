b93a1bba1615f7e9694b4c6781940baa41251dcf
Ariel Blasi <exorcismo@gmail.com>
2018-12-12 18:12:17 -0300

    Add Semantic UI

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import SearchBar from './SearchBar'
+
+export default function App() {
+	return (
+		<div className="App">
+			<SearchBar></SearchBar>
+		</div>
+	);
+}
