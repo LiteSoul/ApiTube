10f557b6eb083fc6cd4994d1e1c2a595217d9565
Ariel Blasi <exorcismo@gmail.com>
2019-01-29 15:47:01 -0300

    add VideoDetail

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 import youtube from '../apis/youtube'
 
 export default function App() {
 	const [videos, setVideos] = useState([])
+	const [selectedVideo, setSelectedVideo] = useState(null)
 
 	const onTermSubmit = async (term) => {
 		console.log(term)
@@ -13,14 +15,18 @@ export default function App() {
 				q: term
 			}
 		})
-
 		setVideos(response.data.items)
+	}
+
+	const onVideoSelect = video => {
+		setSelectedVideo(video)
 	}
 
 	return (
 		<div className="ui container">
 			<SearchBar onTermSubmit={onTermSubmit} />
-			<VideoList videos={videos} />
+			<VideoDetail video={selectedVideo} />
+			<VideoList videos={videos} onVideoSelect={onVideoSelect} />
 		</div>
 	);
 }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './VideoItem.css'
 
-export default function VideoItem({ video }) {
+export default function VideoItem({ video, onVideoSelect }) {
 	return (
-		<div className='video-item item'>
+		<div onClick={() => onVideoSelect(video)} className='video-item item'>
 			<img className='ui image' src={video.snippet.thumbnails.medium.url} alt="" />
 			<div className="content">
 				<div className="header">{video.snippet.title}</div>
